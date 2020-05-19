66090b8c898ac3806eff1a8028ac4a098da1c05f
Khaleb O'Brien <ebukauche52@gmail.com>
2020-05-19 13:40:22 +0100

    hello world

---
+++ Hello_World/main.cpp
@@ -0,0 +1,27 @@
+/**
+ * (weird_coder)
+ * date: mon 13.1.20
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world! am new here" << endl;
+    int a,b,c;
+    a = 15;
+    b = 10;
+    c = 11;
+    //function add() {
+    //    x = a + b;
+    //    return x;
+    //}
+    if (a < 20){
+        cout << "Variable A is not greater than 20" << endl;
+        //add();
+    } else {
+        cout << "Variable A is less than 20" << endl;
+    }
+    return 0;
+}
