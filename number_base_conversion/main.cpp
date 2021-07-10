65ba7b8ad7a2b65debce0423d0f82336f6514fba
Khaleb O'Brien <ebukauche52@gmail.com>
2021-07-10 17:42:16 +0100

    loop

--- number_base_conversion/main.cpp
+++ number_base_conversion/main.cpp
@@ -5,24 +5,56 @@ int binaryToDecimal(string n);
 
 int main()
 {
-    int o = 0;
-    cout << "Please, select an option from the menu below:" << endl;
-    cout << "Press 1 for Binary to Decimal." << endl;
-    cout << "Press 2 for Base 5 to Decimal." << endl;
-    cout << "Press 3 for Octal to Decimal." << endl;
-    cout << "Press 4 for Hexadecimal to Decimal." << endl;
+    int option = 0;
+    do{
+        cout << "Please, select an option from the menu below:" << endl;
+        cout << "Press 1 for Binary to Decimal." << endl;
+        cout << "Press 2 for Base 5 to Decimal." << endl;
+        cout << "Press 3 for Octal to Decimal." << endl;
+        cout << "Press 4 for Hexadecimal to Decimal." << endl;
+        cout << "Press 0 to exit the program" << endl;
 
-    cin >> o;
+        cin >> option;
 
-    switch(o){
-        case 1:
-            string b;
-            cout << "Enter a Binary number:" << endl;
-            cin >> b;
-            cout << binaryToDecimal(b) << endl;
+        switch(option){
+            case 1:{
+                string b;
+                cout << "Enter a Binary number:" << endl;
+                cin >> b;
+                cout << binaryToDecimal(b) << endl;
+                break;
+            }
 
-        //case 2:
+            case 2:{
+                string b;
+                cout << "Enter a number in Base 5:" << endl;
+                cin >> b;
+
+                break;
+            }
+
+            case 3:{
+                string b;
+                cout << "Enter a number in Base 8 (Octal):" << endl;
+                cin >> b;
+
+                break;
+            }
+
+            case 4:{
+                string b;
+                cout << "Enter a number in Base 16 (Hexadecimal):" << endl;
+                cin >> b;
+
+                break;
+            }
+
+            default :
+                cout << "Invalid option selected, please select an option from the list below." << endl;
+                break;
+        }
     }
+    while(option != 0);
 
     return 0;
 }
---
+++ student_score_menu/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Select an action from the menu." << endl;
+    cout << endl;
+    cout <<
+    return 0;
+}
